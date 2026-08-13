4edf49691ed2f958d318baf08c47d77d69dd4546
rushikeshbawke <rushikeshbawke0000@gmail.com>
2026-08-14 00:08:37 +0530

    added the vpc and subnets with their route tables

---
+++ vpc.tf
@@ -0,0 +1,100 @@
+# ---------- VPC ----------
+
+resource "aws_vpc" "main" {
+cidr_block = "12.5.0.0/16"
+enable_dns_support = true
+enable_dns_hostnames = true
+
+tags = {
+Name = "main-vpc"
+}
+
+#------ Internet Gateway ------
+
+resource "aws_internet_gateway" "main" {
+vpc_id = aws_vpc.main.id
+
+tags = {
+Name = "main-igw"
+}
+}
+
+#------ Public Subnet ------
+
+resource "aws_subnet" "public" {
+vpc_id = aws_vpc.main.id
+cidr_block = "12.5.1.0/24"
+map_public_ip_on_launch = true
+availability_zone = "ap-south-1a"
+
+tags = {
+Name = "main-public-subnet"
+}
+}
+
+#----- Private Subnet ------
+
+resource "aws_subnet" "private" {
+vpc_id = aws_vpc.main.id
+cidr_block = "12.5.2.0/24"
+availability_zone = "ap-south-1a"
+
+tags = {
+Name = "main-private-subnet"
+}
+}
+#----- NAT Gateway ------
+
+resource "aws_eip" "nat" {
+domain = "vpc"
+
+tags = {
+Name = "main-nat-eip"
+}
+}
+
+resource "aws_nat_gateway" "main" {
+allocation_id = aws_eip.nat.id
+subnet_id = aws_subnet.public.id
+
+tags = {
+Name = "main-nat-gateway"   
+}
+}
+
+#----- Route Table Public ------
+
+resource "aws_route_table" "public" {
+vpc_id = aws_vpc.main.id
+
+route {
+cidr_block = "0.0.0.0/0"
+gateway_id = aws_internet_gateway.main.id
+}
+
+tags = {
+Name = "main-public-route-table"
+}
+}
+
+resource "aws_route_table_association" "public" {
+subnet_id = aws_subnet.public.id
+route_table_id = aws_route_table.public.id
+}
+
+#----- Route Table Private ------
+resource "aws_route_table" "private" {
+vpc_id = aws_vpc.main.id
+
+route {
+cidr_block = "0.0.0.0/0"
+nat_gateway_id = aws_nat_gateway.main.id
+}
+}
+
+resource "aws_route_table_association" "private" {
+subnet_id = aws_subnet.private.id
+route_table_id = aws_route_table.private.id
+}
+}
+
