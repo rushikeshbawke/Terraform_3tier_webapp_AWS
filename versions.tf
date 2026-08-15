c80e5b3b9d645c3f6c1c3200e452b2edaa376b19
rushikeshbawke <rushikeshbawke0000@gmail.com>
2026-08-16 01:23:11 +0530

    added version and backend for remote state file

---
+++ versions.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "terraform-state-12-5"
+    key            = "terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+
+  provider "aws" {
+    region = "ap-south-1"
+    tags = {
+        ManagedBy = "Terraform"
+  }
+  }
+}
