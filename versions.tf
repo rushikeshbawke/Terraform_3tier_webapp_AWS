e3cfda1a3fce75ac570dc3099603ab2d0e6615a3
rushikeshbawke <rushikeshbawke0000@gmail.com>
2026-08-17 00:20:01 +0530

    added the bootstrap

--- versions.tf
+++ versions.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">= 1.5.0"
+  required_version = ">= 1.0.0"
 
   required_providers {
     aws = {
@@ -16,7 +16,7 @@ terraform {
     bucket         = "terraform-state-12-5"
     key            = "terraform.tfstate"
     region         = "ap-south-1"
-    use_lockfile = "terraform-locks"
+    use_lockfile   = true
     encrypt        = true
   }
 }
